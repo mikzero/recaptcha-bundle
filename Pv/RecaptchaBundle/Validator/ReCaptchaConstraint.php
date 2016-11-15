cbee693f3c6b533f24b36e8d3993ab00e5b13f4a
Pierre Vassoilles <pvassoilles@intuitiv.fr>
2016-11-15 17:38:25 +0100

    Modification du message d'erreur

--- Pv/RecaptchaBundle/Validator/ReCaptchaConstraint.php
+++ Pv/RecaptchaBundle/Validator/ReCaptchaConstraint.php
@@ -21,7 +21,7 @@ use Symfony\Component\Validator\Constraint;
 class ReCaptchaConstraint extends Constraint
 {
 	/** @var string */
-	public $message = 'Veuillez prouver que nous n\'êtes pas un robot';
+	public $message = 'Veuillez prouver que vous n\'êtes pas un robot';
 
 	/** @return string */
 	public function validatedBy()
